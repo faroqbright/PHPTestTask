787cb02cf310b4a3afa543c4cd970cac94d6c5da
Umar Faroq <faroqbrigth@gmail.com>
2021-09-01 19:14:28 +0500

    Test Task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('capsule:cron')->cron('* * * * *');
+        $schedule->command('capsule:cron')->cron('*/3 * * * *');
     }
 
     /**
